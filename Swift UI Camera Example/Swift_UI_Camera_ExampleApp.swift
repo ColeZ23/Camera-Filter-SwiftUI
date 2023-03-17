756468265fe7a67558951b36c0540f07534f8038
ColeZ23 <Cole.Z23@k12.xrds.org>
2023-03-17 09:46:00 -0700

    Update 1

---
+++ Swift UI Camera Example/Swift_UI_Camera_ExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UI_Camera_ExampleApp.swift
+//  Swift UI Camera Example
+//
+//  Created by Cole Zytko on 3/16/23.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UI_Camera_ExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
